183211695417a1340a8460ac03f3918c351248f7
Sushil025 <sushilk1329@gmail.com>
2025-05-05 13:09:41 +0000

    plaindrome code done

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-console.log("Hello")
+// console.log("Hello")
 
-let value  = "deepak";
+let value  = 121;
 
 const checkValue = () => {
-    let newValue = value.toString().split("")
-    console.log(newValue)
+    let newValue = value.toString().split("").reverse().join("");
+    let updatedValue = value.toString()
+
+     if (newValue === updatedValue){
+        console.log(`${newValue} is Palindrome`)
+     } else {
+         console.log(`${newValue} is Not a Palindrome `)
+     }
+
+    // console.log(newValue)
 }
 
 checkValue()
